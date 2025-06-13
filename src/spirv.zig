5a9d14fac48b803d350aa73fad61b0e96f250c8c
Levi Tabosa <silvacostaelevi@gmail.com>
2025-06-13 00:58:52 -0300

    pre-merge

--- src/spirv.zig
+++
@@ -1,7 +0,0 @@
-// This module's purpose is to embed the compiled shader files directly into the executable.
-// The paths are provided by the build.zig script.
-const vert_shader_path = @import("vert_spv_path");
-const frag_shader_path = @import("frag_spv_path");
-
-pub const vert_code = @embedFile(vert_shader_path);
-pub const frag_code = @embedFile(frag_shader_path);
