9cc2425a3bec7d1bd34958e45f01f63d7f8e9d14
Levi Tabosa <silvacostaelevi@gmail.com>
2025-08-02 13:26:46 -0300

    window opens, then crashes

--- build.zig
+++ build.zig
@@ -20,19 +20,12 @@ fn addShaderStep(
     }
 
     compile_step.addArgs(&.{ "--target", "Vulkan-1.3" });
-
-    // Re-execute the compile step if the source file changes.
     compile_step.addFileArg(b.path(source_path));
-
-    // Get a handle to the output artifact.
-    // This will be a new handle if cache miss occurs.
     const output_file_source = compile_step.addOutputFileArg(output_name);
-
     const install_step = b.addInstallFile(
         output_file_source,
         b.fmt("shaders/{s}", .{output_name}),
     );
-
     return &install_step.step;
 }
 
@@ -41,14 +34,19 @@ fn addShaderStep(
 fn configureVulkanAndGlfw(
     module: *std.Build.Module,
     target: std.Build.ResolvedTarget,
-    lib_glfw: *std.Build.Step.Compile,
+    glfw_lib: *std.Build.Step.Compile,
     glfw_dep: *std.Build.Dependency,
     vk_headers_dep: *std.Build.Dependency,
+    vk_loader_dep: *std.Build.Dependency,
+    vk_loader_lib: *std.Build.Step.Compile,
 ) void {
-    module.linkLibrary(lib_glfw);
+    // _ = vk_headers_dep;
+    module.linkLibrary(glfw_lib);
     module.addIncludePath(glfw_dep.path("include"));
     module.addIncludePath(vk_headers_dep.path("include"));
-    module.linkSystemLibrary("vulkan", .{});
+    module.addIncludePath(vk_loader_dep.path("include"));
+    module.addIncludePath(vk_loader_dep.path("loader"));
+    module.linkLibrary(vk_loader_lib);
 
     // Platform-specific libraries
     switch (target.result.os.tag) {
@@ -82,84 +80,92 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const glfw_dep = b.dependency("glfw", .{ .target = target, .optimize = optimize });
-
     const vk_headers_dep = b.dependency("vulkan_headers", .{});
+    const vk_loader_dep = b.dependency("vulkan_loader", .{ .target = target, .optimize = optimize });
     const glslc_dep = b.dependency("glslc", .{ .target = target, .optimize = optimize });
     const glslc_exe = glslc_dep.artifact("shader_compiler");
 
-    // --- Compile GLFW as a Static Library ---
     const lib_glfw = b.addStaticLibrary(.{ .name = "glfw", .target = target, .optimize = optimize });
     lib_glfw.linkLibC();
     lib_glfw.addIncludePath(glfw_dep.path("include"));
     lib_glfw.addCSourceFiles(.{
         .root = glfw_dep.path("src"),
         .files = &.{
-            "context.c",
-            "init.c",
-            "input.c",
-            "monitor.c",
-            "vulkan.c",
-            "window.c",
-            "osmesa_context.c",
-            "platform.c",
-            "egl_context.c",
-            "null_init.c",
-            "null_monitor.c",
-            "null_joystick.c",
+            "context.c",        "init.c",     "input.c",       "monitor.c",   "vulkan.c",       "window.c",
+            "osmesa_context.c", "platform.c", "egl_context.c", "null_init.c", "null_monitor.c", "null_joystick.c",
             "null_window.c",
         },
     });
     if (target.result.os.tag == .linux) {
         lib_glfw.addCSourceFiles(.{ .root = glfw_dep.path("src"), .files = &.{
-            "x11_init.c",
-            "x11_monitor.c",
-            "x11_window.c",
-            "xkb_unicode.c",
-            "posix_time.c",
-            "posix_thread.c",
-            "posix_module.c",
-            "posix_poll.c",
-            "glx_context.c",
-            "linux_joystick.c",
+            "x11_init.c",    "x11_monitor.c",    "x11_window.c",   "xkb_unicode.c",
+            "posix_time.c",  "posix_thread.c",   "posix_module.c", "posix_poll.c",
+            "glx_context.c", "linux_joystick.c",
         } });
         lib_glfw.root_module.addCMacro("_GLFW_X11", "1");
-        lib_glfw.linkSystemLibrary("X11");
-        lib_glfw.linkSystemLibrary("Xrandr");
-        lib_glfw.linkSystemLibrary("Xinerama");
-        lib_glfw.linkSystemLibrary("Xi");
-        lib_glfw.linkSystemLibrary("Xcursor");
-        lib_glfw.linkSystemLibrary("Xxf86vm");
     } else if (target.result.os.tag == .windows) {
         lib_glfw.addCSourceFiles(.{ .root = glfw_dep.path("src"), .files = &.{
-            "win32_init.c",
-            "win32_joystick.c",
-            "win32_monitor.c",
-            "win32_time.c",
-            "win32_thread.c",
-            "win32_window.c",
+            "win32_init.c", "win32_joystick.c", "win32_monitor.c",
+            "win32_time.c", "win32_thread.c",   "win32_window.c",
         } });
         lib_glfw.root_module.addCMacro("_GLFW_WIN32", "1");
         lib_glfw.linkSystemLibrary("gdi32");
         lib_glfw.linkSystemLibrary("shell32");
     } else if (target.result.os.tag == .macos) {
         lib_glfw.addCSourceFiles(.{ .root = glfw_dep.path("src"), .files = &.{
-            "cocoa_init.m",
-            "cocoa_joystick.m",
-            "cocoa_monitor.m",
-            "cocoa_time.m",
-            "cocoa_window.m",
+            "cocoa_init.m", "cocoa_joystick.m", "cocoa_monitor.m",
+            "cocoa_time.m", "cocoa_window.m",
         } });
         lib_glfw.root_module.addCMacro("_GLFW_COCOA", "1");
     }
 
-    // This module is created and configured with all C dependencies.
-    // Any executable that imports "c" will now automatically get all the correct
-    // include paths and library links.
+    const lib_vulkan_loader = b.addStaticLibrary(.{ .name = "vulkan-loader", .target = target, .optimize = optimize });
+
+    lib_vulkan_loader.linkLibC();
+    lib_vulkan_loader.addIncludePath(vk_headers_dep.path("include"));
+    lib_vulkan_loader.addIncludePath(vk_loader_dep.path("loader"));
+    lib_vulkan_loader.addIncludePath(vk_loader_dep.path("loader/generated"));
+
+    lib_vulkan_loader.root_module.addCMacro("VULKAN_LOADER_STATIC_LIB", "1");
+    lib_vulkan_loader.root_module.addCMacro("FALLTHROUGH_SUPPORTED", "1");
+
+    // This enables beta extensions like the ones for CUDA, fixing the VK_OBJECT_TYPE errors.
+    lib_vulkan_loader.root_module.addCMacro("VK_ENABLE_BETA_EXTENSIONS", "1");
+
+    if (target.result.os.tag == .linux) {
+        // These paths are normally configured by CMake. We provide standard defaults for Linux.
+        lib_vulkan_loader.root_module.addCMacro("SYSCONFDIR", "\"/etc\"");
+        lib_vulkan_loader.root_module.addCMacro("FALLBACK_CONFIG_DIRS", "\"/etc/xdg\"");
+        lib_vulkan_loader.root_module.addCMacro("FALLBACK_DATA_DIRS", "\"/usr/local/share:/usr/share\"");
+
+        // The loader's source code checks for these via #ifdef to include the right system headers.
+        lib_vulkan_loader.root_module.addCMacro("HAVE_SYS_STAT_H", "1");
+        lib_vulkan_loader.root_module.addCMacro("HAVE_XCB_H", "1");
+        lib_vulkan_loader.root_module.addCMacro("HAVE_STDATOMIC_H", "1");
+    } else if (target.result.os.tag == .windows) {
+        // Suppresses warnings about using "unsafe" C functions on Windows.
+        lib_vulkan_loader.root_module.addCMacro("_CRT_SECURE_NO_WARNINGS", "NULL");
+    } else if (target.result.os.tag == .macos) {
+        lib_vulkan_loader.root_module.addCMacro("SYSCONFDIR", "\"/etc\"");
+        lib_vulkan_loader.root_module.addCMacro("HAVE_SYS_STAT_H", "1");
+        lib_vulkan_loader.root_module.addCMacro("HAVE_STDATOMIC_H", "1");
+    }
+
+    lib_vulkan_loader.addCSourceFiles(.{
+        .root = vk_loader_dep.path("loader"),
+        .files = &.{
+            "loader.c",           "allocation.c",         "unknown_function_handling.c", "trampoline.c",
+            "terminator.c",       "wsi.c",                "log.c",                       "cJSON.c",
+            "loader_json.c",      "loader_environment.c", "settings.c",                  "dev_ext_trampoline.c",
+            "extension_manual.c", "gpa_helper.c",         "debug_utils.c",
+        },
+    });
+
     const c_mod = b.createModule(.{
         .root_source_file = b.path("src/c/c.zig"),
         .target = target,
     });
-    configureVulkanAndGlfw(c_mod, target, lib_glfw, glfw_dep, vk_headers_dep);
+    configureVulkanAndGlfw(c_mod, target, lib_glfw, glfw_dep, vk_headers_dep, vk_loader_dep, lib_vulkan_loader);
 
     const shaders = [_]struct { name: []const u8, path: []const u8 }{
         .{ .name = "gui", .path = "src/shaders/code/gui" },
@@ -169,7 +175,6 @@ pub fn build(b: *std.Build) !void {
         .{ .name = "test", .path = "src/shaders/code/test" },
     };
 
-    // Compile all shaders and collect their install steps.
     var shader_install_steps = std.ArrayList(*std.Build.Step).init(b.allocator);
     defer shader_install_steps.deinit();
 
@@ -178,32 +183,17 @@ pub fn build(b: *std.Build) !void {
         const frag_source = b.fmt("{s}/{s}.frag", .{ shader.path, shader.name });
         const vert_output = b.fmt("{s}.vert.spv", .{shader.name});
         const frag_output = b.fmt("{s}.frag.spv", .{shader.name});
-
         const install_vert_step = addShaderStep(b, glslc_exe, optimize, vert_source, vert_output);
         const install_frag_step = addShaderStep(b, glslc_exe, optimize, frag_source, frag_output);
-
         try shader_install_steps.append(install_vert_step);
         try shader_install_steps.append(install_frag_step);
     }
 
-    // TODO: add shader binaries as anonymous imports
-    // and generate spirv.zig automatically.
-    // This will have the same effect but binaries won't need
-    // to be in the package directory.
     const spirv_options = b.addOptions();
-    spirv_options.addOption(
-        []const u8,
-        "out_dir",
-        b.fmt("{s}/shaders/", .{b.install_prefix}),
-    );
-    const spirv_mod = b.createModule(.{
-        .root_source_file = b.path("spirv.zig"),
-        .target = target,
-    });
-
+    spirv_options.addOption([]const u8, "out_dir", b.fmt("{s}/shaders/", .{b.install_prefix}));
+    const spirv_mod = b.createModule(.{ .root_source_file = b.path("spirv.zig"), .target = target });
     spirv_mod.addOptions("shaders", spirv_options);
 
-    // Define and Build Executables
     const execs = [_]struct { []const u8, []const u8 }{
         .{ "triangle", "example/main.zig" },
         .{ "example", "example/example.zig" },
@@ -221,19 +211,15 @@ pub fn build(b: *std.Build) !void {
 
         exe.root_module.addImport("c", c_mod);
         exe.root_module.addImport("spirv", spirv_mod);
-
         exe.root_module.addAnonymousImport("font", .{ .root_source_file = b.path("src/fonts/font.zig") });
         exe.root_module.addAnonymousImport("png", .{ .root_source_file = b.path("src/png/png_helper.zig") });
-        // TODO: Make this import a scene interface instead so scenes can be user code
         exe.root_module.addAnonymousImport("geometry", .{ .root_source_file = b.path("src/scenes/geometry.zig") });
         exe.root_module.addAnonymousImport("util", .{ .root_source_file = b.path("src/util/util.zig") });
 
-        // Shaders should be installed before compiling the executable.
         for (shader_install_steps.items) |shader_step| {
             exe.step.dependOn(shader_step);
         }
 
-        // For now we run it emediately after building.
         const install = b.addInstallArtifact(exe, .{});
         const run_cmd = b.addRunArtifact(exe);
         run_cmd.step.dependOn(&install.step);
